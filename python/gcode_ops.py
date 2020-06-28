af27ecd39a7057f29c58b9340d201d5340632388
Miguel Agueda <40192229+MiguelAgueda@users.noreply.github.com>
2020-06-28 22:45:48 +0100

    Added functions for generating g-code.

--- python/gcode_ops.py
+++ python/gcode_ops.py
@@ -1,13 +1,45 @@
-class gcoderizer:
-    def __init__(self):
-        self.canvas_dim_x = 0
-        self.canvas_dim_y = 0
-        self.plotter_dim_x = 0
-        self.plotter_dim_y = 0
-        self.scale_x = self.plotter_dim_x / self.canvas_dim_x 
-        self.scale_y = self.plotter_dim_y / self.canvas_dim_y 
+class Gcoderizer:
+    """
+    The `Gcoderizer` class handles the creation and formation of g-code commands.
+    G-code is generated to be machine specific in terms of type and size.
 
-    def gen_gcode(self, start_x, start_y, end_x, end_y):
+    Attributes:
+        canvas_dim_(x/y) - Dimensions of canvas, using canvas units (px).
+        plotter_dim(x/y) - Dimensions of plotter, using plotter units (m, mm, in).
+    """
+    def __init__(self):
+        self.canvas_dim_x = 1
+        self.canvas_dim_y = 1
+        self.plotter_dim_x = 1
+        self.plotter_dim_y = 1
+        self.scale_x = self.plotter_dim_x / self.canvas_dim_x 
+        self.scale_y = self.plotter_dim_y / self.canvas_dim_y
+    
+
+    def _in_canvas_bounds(x_points, y_points):
+        """
+        Ensure that machine will not be given instructions to move beyond it's bounds.
+        If x-y-points are within bounds, proper scaling will ensure that the
+        plotter position is always within it's limits.
+        Parameters:
+            x_points, y_points - X-y-points to compare to canvas boundaries.
+        
+        Returns:
+            True - If x-y-points are within the bounds of the canvas.
+            False - If x-y-points are out of the canvas' bounds.
+        """
+        try:
+            assert min(x_points) >= 0
+            assert min(y_points) >= 0
+            assert max(x_points) <= self.canvas_dim_x
+            assert max(y_points) <= self.canvas_dim_y
+        except:
+            return False
+
+        return True
+
+
+    def gen_line(self, start_x, start_y, end_x, end_y):
         """
         Generate a Gcode command to move from `start_pt` to `end_pt`.
         Parameters:
@@ -17,14 +49,25 @@ class gcoderizer:
             Gcode commands recreating the line in the plotter's frame.
         """
 
-        plotter_start_x = start_x * self.scale_x
-        plotter_start_y = start_y * self.scale_y
-        plotter_end_x = end_x * self.scale_x
-        plotter_end_y = end_y * self.scale_y
+        if not self._in_canvas_bounds([start_x, end_x], [start_y, end_y]):
+            print("Illegal canvas boundary supplied as parameter.\n")
+            quit()
 
-        gcode_command = F"G0 X{plotter_start_x} Y{plotter_start_y}\n"
-        ## gcode_command += some command to activate/drop the plotter carriage.
-        gcode_command += F"G1 X{plotter_end_x} Y{plotter_end_y}"
-        ## gcode_command += some command to deactivate/lift the plotter carriage.
+        plotter_start_x = round(start_x * self.scale_x, 1)
+        plotter_start_y = round(start_y * self.scale_y, 1)
+        plotter_end_x = round(end_x * self.scale_x, 1)
+        plotter_end_y = round(end_y * self.scale_y, 1)
+
+        gcode_command = F"G0 Z10\n"
+        gcode_command += F"G0 X{plotter_start_x} Y{plotter_start_y}\n"
+        gcode_command += F"G0 Z2\n"
+        gcode_command += F"G1 X{plotter_end_x} Y{plotter_end_y}\n"
+        gcode_command += F"G0 Z10\n"
 
         return gcode_command
+
+    def home(self):
+        return "G28"
+
+    def stop(self):
+        return "M18"
